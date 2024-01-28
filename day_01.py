5a9f3242c6c278cddd8feeb070c181cf1f36e3c1
Jackie Liu <jackolake@gmail.com>
2024-01-29 01:31:03 +0800

    Day 1 - Multiple instructions

--- day_01.py
+++ day_01.py
@@ -11,23 +11,26 @@ position_x, position_y = 0, 0
 current_direction = 'N'
 
 # Sample Input
-left_right = 'R'
-steps = 2
+input_text = 'R2, R2, R2'
+for instruction in input_text.split(', '):
+    left_right = instruction[0]     # 'L' or 'R'
+    step_text = instruction[1:]     # '2'
+    steps = int(step_text)
 
-# Execute the input
-new_direction = direction_map[current_direction][left_right]
-if new_direction == 'N':
-    position_y = position_y + steps     # Go north
-elif new_direction == 'S':              
-    position_y = position_y - steps     # Go south
-elif new_direction == 'E':
-    position_x = position_x + steps     # Go east
-elif new_direction == 'W':
-    position_x = position_x - steps     # Go west
-else:
-    print(f'Wrong direction: {new_direction}')
-# Record the new direction
-current_direction = new_direction
+    # Execute the instruction
+    new_direction = direction_map[current_direction][left_right]
+    if new_direction == 'N':
+        position_y = position_y + steps     # Go north
+    elif new_direction == 'S':              
+        position_y = position_y - steps     # Go south
+    elif new_direction == 'E':
+        position_x = position_x + steps     # Go east
+    elif new_direction == 'W':
+        position_x = position_x - steps     # Go west
+    else:
+        print(f'Wrong direction: {new_direction}')
+    # Record the new direction
+    current_direction = new_direction
 
 # Print result
 print(f'Ending position: ({position_x}, {position_y})')
